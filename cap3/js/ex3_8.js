62b0d070444a894fd2bf2b015ef5873e113781ae
cristianolimaribeiro <cristiano.lima.ribeiro@gmail.com>
2021-10-08 16:39:43 -0300

    exercicio radar de velocidade

---
+++ cap3/js/ex3_8.js
@@ -0,0 +1,39 @@
+function radar() {
+    var inVelocidadePermitida = document.getElementById("inVelocidadePermitida")
+    var outVelocidadeObtida = document.getElementById("outVelocidadeObtida");
+    var outMensagem = document.getElementById("outMensagem");
+    var outSituacao = document.getElementById("outSituacao");
+
+    var velocidadePermitida = Number(inVelocidadePermitida.value);
+
+    if (inVelocidadePermitida.value == '' || isNaN(velocidadePermitida)) {
+        outMensagem.textContent = 'Calibre com a Velocidade'
+        outMensagem.style.color = 'red';
+    } else {
+
+        var velocidade = Math.floor(Math.random() * 100) + 50;
+
+        var velocidadeMenor = ((velocidadePermitida) / 100) + velocidadePermitida;
+        var velocidadeMaior = ((velocidadePermitida * 20) / 100) + velocidadePermitida;
+
+        outVelocidadeObtida.textContent = 'Velocidade: ' + velocidade + ' KM/h '
+
+
+        if (outMensagem.textContent != '') {
+            outMensagem.textContent = '';
+        }
+        if (velocidade > velocidadeMenor && velocidade < velocidadeMaior) {
+            outSituacao.textContent = 'Situação: Multa Leve'
+        }
+        else if (velocidade > velocidadeMaior) {
+            outSituacao.textContent = 'Situação: Multa Grave'
+        } else if (velocidade < velocidadePermitida) {
+            outSituacao.textContent = 'Carro não Multado'
+        }
+    }
+
+
+}
+
+var btnCapturarRadar = document.getElementById("btnCapturarRadar");
+btnCapturarRadar.addEventListener('click', radar)
